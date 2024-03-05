0a0cea2fc71168df5e1cc8f7175ac0bb410d5890
Reneuit <Reneuit@DESKTOP-MMOB49H>
2024-03-05 23:26:51 +0530

    creating customer module

---
+++ github project/src/CreateCustomer.java
@@ -0,0 +1,19 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class CreateCustomer {
+
+	public class CustomerModule {
+		@Test
+		public void creatCustomer()
+		
+		{
+			Reporter.log("Create customer",true);
+			
+		}
+	}
+}
+		
+	
